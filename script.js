f3a9a632a3ac8a516d02c3afe9250e18f0a29893
Elshadai Tegegn <54620095+ElshadaiK@users.noreply.github.com>
2020-06-19 15:50:22 +0300

    Fixed few bugs

--- script.js
+++ script.js
@@ -1,30 +1,31 @@
 function game(){
-    var id= setInterval(fetch,3)
+    var id= setInterval(alter,3)
     
-    function fetch(){
-        var operand1 = parseInt(document.querySelectorAll("#task_x")[0].innerText)
-        var operand2 =  parseInt(document.querySelectorAll("#task_y")[0].innerText)
-        var operator = document.querySelectorAll("#task_op")[0].innerText
-        var answer =  parseInt(document.querySelectorAll("#task_res")[0].innerText)
-        var real=0
+    function alter(){
+        var operand1 = parseInt(document.querySelectorAll("#task_x")[0].innerText);
+        var operand2 =  parseInt(document.querySelectorAll("#task_y")[0].innerText);
+        var operator = document.querySelectorAll("#task_op")[0].innerText;
+        var answer =  parseInt(document.querySelectorAll("#task_res")[0].innerText);
+        var result = 0;
 
         if (operator == "-"){
-             result = operand1 - operand2 
+             result = operand1 - operand2;
             }
         else if (operator == "+"){
-            result = operand1 + operand2 
+            result = operand1 + operand2;
             }
         else if (operator == "/"){
-            result = operand1 / operand2 
+            result = operand1 / operand2;
              }
         else {
-            result = operand1 * operand2 
+            result = operand1 * operand2;
             }
         
         if (result == answer){
-            document.querySelectorAll("#button_correct")[0].click()
+            document.querySelectorAll("#button_correct")[0].click();
         }
         else{
-            document.querySelectorAll("#button_wrong")[0].click()}
+            document.querySelectorAll("#button_wrong")[0].click();
+        };
         
     }}
